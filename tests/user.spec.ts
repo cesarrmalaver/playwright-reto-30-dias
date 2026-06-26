deb6b7a38a7f2594dca6c45852be923cc48aa00a
Cesar Malaver <cesarrmalaver@gmail.com>
2026-06-25 22:09:10 -0500

    Primera parte reto 02

---
+++ tests/user.spec.ts
@@ -0,0 +1,28 @@
+import test from "@playwright/test";
+
+
+test('Get all usernames Registered',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin');
+    await page.getByRole('textbox',{name:'Password'}).fill('admin123');
+    await page.getByRole('button',{name:'Login'}).click();
+
+    await page.getByRole('link',{name:'Admin'}).click();
+
+    await page.getByRole('navigation',{name:'Topbar Menu'}).getByText('User Management ').click();
+    await page.getByRole('menuitem',{name:'Users'}).click();
+    
+    const rows = page.getByRole('table').getByRole('row')
+    const usernames: string[] = []
+    const rowCounts = await rows.count()
+
+
+    for (let i=1;i<rowCounts;i++){
+        const cell = rows.nth(i).getByRole('cell').nth(1)
+        const username = await cell.textContent()
+        if(username){
+            usernames.push(username)
+        }
+    }
+    console.log(usernames)
+})
